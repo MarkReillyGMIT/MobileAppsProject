8c5a7777be6ee763713b423ffc91f28c10e4c452
Mark Reilly <37144811+MarkReillyGMIT@users.noreply.github.com>
2020-03-07 21:36:06 +0000

    Add Player Movement

---
+++ Assets/__Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // == public fields ==
+
+    // == private fields ==
+    private Rigidbody2D rb;
+
+    private Camera gameCamera;
+
+    [SerializeField] private float speed = 5.0f;
+
+    // == private methods ==
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // add hMovement
+        // if the player presses the up arrow, then move
+        float vMovement = Input.GetAxis("Vertical");
+        float hMovement = Input.GetAxis("Horizontal");
+        // apply a force, get the player moving.
+        rb.velocity = new Vector2(hMovement * speed,
+                                  vMovement * speed);
+        // keep the player on the screen
+        // float xValue = Mathf.clamp(value, min, max)
+        // rb.position.x 
+        float yValue = Mathf.Clamp(rb.position.y, -6.0f, 6.0f);
+        float xValue = Mathf.Clamp(rb.position.x, -11.0f, 11.0f);
+
+        rb.position = new Vector2(xValue, yValue);
+    }
+}
