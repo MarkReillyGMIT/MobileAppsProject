b72b33d327117b31705e193035bf0ff14f89d937
Mark Reilly <37144811+MarkReillyGMIT@users.noreply.github.com>
2020-03-11 16:05:17 +0000

    Added scrolling background

---
+++ Assets/__Scripts/ScrollingBackground.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingBackground : MonoBehaviour
+{
+    public float bgSpeed;
+    public Renderer bgRend;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bgRend.material.mainTextureOffset += new Vector2(bgSpeed * Time.deltaTime, 0f);
+    }
+}
